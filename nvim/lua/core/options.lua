7e50a46ed55abb9cc4f5e3edd389519471ac0143
idkosilov <idkosilov@gmail.com>
2025-02-10 18:54:18 +0700

    Update terminal and pyright configuration

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -41,3 +41,18 @@ vim.opt.shortmess:append 'c' -- don't give |ins-completion-menu| messages
 vim.opt.iskeyword:append '-' -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
+
+-- Terminal settings
+function OpenTerminal()
+  vim.cmd("botright 10split | terminal")
+  vim.opt.number = false
+  vim.opt.relativenumber = false
+  vim.cmd("startinsert")
+end
+
+
+vim.api.nvim_set_keymap("n", "<leader>t", ":lua OpenTerminal()<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>:hide<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>x", ":hide<CR>", { noremap = true, silent = true })
+
