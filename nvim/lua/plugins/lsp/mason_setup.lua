2617744ef3360f72f582109b848709db7c91adce
idkosilov <idkosilov@gmail.com>
2025-02-10 22:15:20 +0700

    Update lsp and theme

--- nvim/lua/plugins/lsp/mason_setup.lua
+++ nvim/lua/plugins/lsp/mason_setup.lua
@@ -3,10 +3,8 @@ local servers_config = require('plugins.lsp.servers').servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
--- Настройка Mason
 require('mason').setup()
 
--- Установка инструментов (например, stylua, а также LSP-серверы, перечисленные в таблице)
 local ensure_installed = vim.tbl_keys(servers_config)
 table.insert(ensure_installed, 'stylua')
 
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -15,26 +15,5 @@ return {
 
     -- Load the colorscheme
     require('nord').set()
-
-    -- Function to set menu borders to transparent
-    -- local set_menu_border_transparency = function()
-    --   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', fg = 'NONE' })
-    --   vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE', fg = 'NONE' })
-    -- end
-
-    -- Execute the function once after loading the colorscheme
-    -- set_menu_border_transparency()
-
-    local bg_transparent = true
-
-    -- Toggle background transparency
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd [[colorscheme nord]]
-      -- set_menu_border_transparency()
-    end
-
-    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
   end,
 }
